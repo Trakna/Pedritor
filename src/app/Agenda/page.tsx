44c735a8598368ad10215f69e8251d4ea7b4e639
João Pedro <jp.joaopedroas@gmail.com>
2025-08-06 22:38:43 -0300

    Updatate in agenda

--- src/app/Agenda/page.tsx
+++ src/app/Agenda/page.tsx
@@ -2,7 +2,11 @@ export default function Agenda() {
   return (
     <div>
       <h1>Agenda</h1>
-      <p className=" bg-white text-black">Bem-vindo à nossa aplicação!</p>
+      <p className=" bg-white text-black">Bem-vindo à nossa Agenda!
+
+        Horários personalizados para você, com base no seu perfil e preferências.
+        <br />
+      </p>
     </div>
   );
 }
