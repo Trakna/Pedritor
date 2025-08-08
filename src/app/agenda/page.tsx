e4f6833319c27e031c5a4164285c7e5b6bbf5927
João Pedro <jp.joaopedroas@gmail.com>
2025-08-07 23:07:12 -0300

    Landing Page Correction

--- src/app/agenda/page.tsx
+++ src/app/agenda/page.tsx
@@ -1,14 +1,10 @@
-import { Hero } from "../components/hero/hero";
-import { FeatureList } from "../components/feature-list/feature-list";
-import { Contatos } from "../components/contacts/contacts";
-
 
 export default function Agenda() {
   return (
     <>
-      <Hero></Hero>
-      <FeatureList></FeatureList>
-      <Contatos></Contatos>
+      <p>
+        Está é a Agenda Inicial, teremos outras alterações aqui
+      </p>
       </>
       );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import { Hero } from "./components/hero/hero";
+import { FeatureList } from "./components/feature-list/feature-list";
+import { Contatos } from "./components/contacts/contacts";
 
-export default function HelloWorld(){
-    return(
-        <h1>page.tsx Mudar depois</h1>
-    )
-}
+
+export default function home() {
+  return (
+    <>
+      <Hero></Hero>
+      <FeatureList></FeatureList>
+      <Contatos></Contatos>
+      </>
+      );
+};
