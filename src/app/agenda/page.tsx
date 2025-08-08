fe7161288959ca686e90b931c7df7b48e1ec77f2
João Pedro <jp.joaopedroas@gmail.com>
2025-08-07 21:16:15 -0300

    Alterações de Case Sensitive

---
+++ src/app/agenda/page.tsx
@@ -0,0 +1,12 @@
+export default function Agenda() {
+  return (
+    <div>
+      <h1>Agenda</h1>
+      <p className=" bg-white text-black">Bem-vindo à nossa Agenda!
+
+        Horários personalizados para você, com base no seu perfil e preferências.
+        <br />
+      </p>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Header } from "./components/header/header";
 import { Hero } from "./components/hero/hero";
 import { FeatureList } from "./components/feature-list/feature-list";
 import { Contatos } from "./components/contacts/contacts";
-import { Footer } from "./components/footer/footer";
+import { Footer } from "./components/footer/Footer";
 import "./globals.css";
 
 
