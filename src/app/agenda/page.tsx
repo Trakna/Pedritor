ac8465249039e2dc00c82c74b58ca9ba052bcd02
João Pedro <jp.joaopedroas@gmail.com>
2025-08-19 23:43:06 -0300

    update agenda

--- src/app/agenda/page.tsx
+++ src/app/agenda/page.tsx
@@ -8,6 +8,7 @@ export default function Agenda() {
       </p>
       <p className="text-center text-lg text-gray-600">
         Fique atento às atualizações!
+        teste dois
       </p>
     </section>
       </>
